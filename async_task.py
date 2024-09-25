330580bb452c31de1996fe97d357109c0fc718a3
Pang Narithtithya <157789676+Tithya-Pang@users.noreply.github.com>
2024-09-25 12:10:21 +0700

    done
    I already upload it before the deadline but I can't see my file that's why I upload this again

--- async_task.py
+++ async_task.py
@@ -1,7 +1,19 @@
 import asyncio
 
+#Function to
 async def async_write_to_file(filename, data, duration):
-    pass
+    await asyncio.sleep(duration)
+    try:
+        with open(filename, 'w') as file:
+            file.write("\n".join(map(str, data)))
+        print(f"Finished writing to {filename}")
+    except Exception as e:
+        print(f"Error writing to {filename}: {e}")
 
 async def run_async_tasks():
-    pass
+    data = [2,3,5,7]
+    await asyncio.gather(
+        async_write_to_file("primes.txt", data, 2),
+        async_write_to_file("primes2.txt", data, 4)
+    )
+
